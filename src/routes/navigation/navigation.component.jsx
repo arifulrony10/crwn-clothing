93c5fc7ad3892dc1bc8ed199cbbdff0a22ae9216
Md. Ariful Islam <hi@ariful.me>
2023-05-28 23:42:23 +0600

    firebase setting up

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
+import './navigation.styles.scss';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link className='logo-container' to={'/'}>
+          <CrwnLogo className='logo' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link to={'/shop'} className='nav-link'>
+            SHOP
+          </Link>
+          <Link to={'/sing-in'} className='nav-link'>
+            Sing In
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+export default Navigation;
